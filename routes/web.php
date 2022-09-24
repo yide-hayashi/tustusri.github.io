e2ae2f73d55f11aaf1526f876be0dc973812c7b3
つつり <a8849288@hotmail.com>
2022-09-25 00:54:51 +0900

    add auth

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Api;
+use Illuminate\Support\Facades\Auth;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,39 +16,44 @@ use App\Http\Controllers\Api;
 |
 */
 
-Route::get('/',"index@indexPage");
+Route::get('/',"index@indexPage")->middleware("guest");
 
 Route::post("/managerpage/upload","Api\HomePageController@updateImg");
 
 Route::group(["prefix"=>"managerpage"],function()
 {
+    
     Route::get('/',"ManagePage@manageIndex");
-    Route::put("/updateTitle/{id}","Api\HomePageController@update");
-
-
-    Route::put("/store","Api\protfolio@store");
-    Route::post("/create","Api\protfolio@create");
-
-    Route::get('/createProject',"Api\protfolio@creatshow");
-    Route::get('/UpdateProject',"Api\protfolio@show");
     
-    Route::put("/storeContent","Api\protfolio@storeContent");
-    Route::post("/createContent","Api\protfolio@createContentPage");
-    Route::delete("/del/{pjn}","Api\protfolio@destroy");
+      Route::middleware("auth")->group(function(){
+        Route::put("/updateTitle/{id}","Api\HomePageController@update");
 
+
+        Route::put("/store","Api\protfolio@store");
+        Route::post("/create","Api\protfolio@create");
     
+        Route::get('/createProject',"Api\protfolio@creatshow");
+        Route::get('/UpdateProject',"Api\protfolio@show");
     
+        Route::put("/storeContent","Api\protfolio@storeContent");
+        Route::post("/createContent","Api\protfolio@createContentPage");
+        Route::delete("/del/{pjn}","Api\protfolio@destroy");
+    
+        
+        
+    
+        Route::put("/aboutme/update","Api\aboutmeController@update");
+        Route::get("/aboutme","Api\aboutmeController@showAboutMeContent");
+    
+        Route::post("/aboutme/create","Api\aboutmeController@create");
+        Route::put("/aboutme/updateHistory/{id}","Api\aboutmeController@updateHistory");
+        Route::get("/aboutme/showAboutMeBeUpdatedContent/{id}","Api\aboutmeController@showAboutMeBeUpdatedContent");
+        Route::delete("/aboutme/del/{id}","Api\aboutmeController@destroy");
+    
+        Route::get("/Contact/show","Api\contactController@show");
+        Route::put("/Contact/update","Api\contactController@update");
+      });
 
-    Route::put("/aboutme/update","Api\aboutmeController@update");
-    Route::get("/aboutme","Api\aboutmeController@showAboutMeContent");
-
-    Route::post("/aboutme/create","Api\aboutmeController@create");
-    Route::put("/aboutme/updateHistory/{id}","Api\aboutmeController@updateHistory");
-    Route::get("/aboutme/showAboutMeBeUpdatedContent/{id}","Api\aboutmeController@showAboutMeBeUpdatedContent");
-    Route::delete("/aboutme/del/{id}","Api\aboutmeController@destroy");
-
-    Route::get("/Contact/show","Api\contactController@show");
-    Route::put("/Contact/update","Api\contactController@update");
 });
 
 Route::group(["prefix"=>"user"],function(){ //同一層的可以寫在一起
@@ -61,7 +68,9 @@ Route::group(["prefix"=>"user"],function(){ //同一層的可以寫在一起
     });
 });
 
-
-Route::get('/t',function(){
-    var_dump(public_path(),storage_path());
+Route::get("/login",function(){
+    return redirect("/");
 });
+Route::get('/t',function(){
+    var_dump(Auth::check());
+})->middleware("auth");
